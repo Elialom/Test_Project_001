b9b778ac264e0389300eac9aa782ed28c3f21666
nikita <gigtans@gmail.com>
2023-01-25 21:17:23 +0300

    small refactoring code

--- app/src/main/java/com/example/test_project_001/TicTacToeActivity.kt
+++ app/src/main/java/com/example/test_project_001/TicTacToeActivity.kt
@@ -5,6 +5,9 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class TicTacToeActivity : AppCompatActivity() {
+
+    var stateField: List<PlayableCell> = emptyList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tic_tac_toe_activity)
@@ -18,7 +21,83 @@ class TicTacToeActivity : AppCompatActivity() {
         val button8 = findViewById<Button>(R.id.tic_tac_toe_btn_8)
         val button9 = findViewById<Button>(R.id.tic_tac_toe_btn_9)
         val restartButton = findViewById<Button>(R.id.tic_tac_toe_start)
-        val stateField = mutableListOf<PlayableCell>(
+
+        createEmptyState()
+
+        button1.setOnClickListener() {
+            showGameField()
+            button1.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 0 && it.y == 0 }
+            stateField.find { it.x == 0 && it.y == 0 }?.state = State.PLAYER
+            showGameField()
+        }
+        button2.setOnClickListener() {
+            showGameField()
+            button2.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 1 && it.y == 0 }
+            stateField.find { it.x == 1 && it.y == 0 }?.state = State.PLAYER
+            showGameField()
+        }
+        button3.setOnClickListener() {
+            showGameField()
+            button3.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 2 && it.y == 0 }
+            stateField.find { it.x == 2 && it.y == 0 }?.state = State.PLAYER
+            showGameField()
+        }
+        button4.setOnClickListener() {
+            showGameField()
+            button4.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 0 && it.y == 1 }
+            stateField.find { it.x == 0 && it.y == 1 }?.state = State.PLAYER
+            showGameField()
+        }
+        button5.setOnClickListener() {
+            showGameField()
+            button5.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 1 && it.y == 1 }
+            stateField.find { it.x == 1 && it.y == 1 }?.state = State.PLAYER
+            showGameField()
+        }
+        button6.setOnClickListener() {
+            showGameField()
+            button6.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 2 && it.y == 1 }
+            stateField.find { it.x == 2 && it.y == 1 }?.state = State.PLAYER
+            showGameField()
+        }
+        button7.setOnClickListener() {
+            showGameField()
+            button7.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 0 && it.y == 2 }
+            stateField.find { it.x == 0 && it.y == 2 }?.state = State.PLAYER
+            showGameField()
+        }
+        button8.setOnClickListener() {
+            showGameField()
+            button8.setText(R.string.tic_tac_toe_x)
+            stateField.filter { it.x == 1 && it.y == 2 }
+            stateField.find { it.x == 1 && it.y == 2 }?.state = State.PLAYER
+            showGameField()
+        }
+
+        button9.setOnClickListener() {
+            showGameField()
+
+            stateField.filter { it.x == 2 && it.y == 2 }
+            stateField.find { it.x == 2 && it.y == 2 }?.state = State.PLAYER
+            showGameField()
+        }
+
+
+
+        restartButton.setOnClickListener() {
+           createEmptyState()
+        }
+    }
+
+    private fun createEmptyState() {
+        stateField = mutableListOf<PlayableCell>(
             PlayableCell(0, 0, State.EMPTY),
             PlayableCell(1, 0, State.EMPTY),
             PlayableCell(2, 0, State.EMPTY),
@@ -29,92 +108,9 @@ class TicTacToeActivity : AppCompatActivity() {
             PlayableCell(1, 2, State.EMPTY),
             PlayableCell(2, 2, State.EMPTY)
         )
-        button1.setOnClickListener() {
-            showGameField(stateField)
-            button1.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 0 && it.y == 0 }
-            stateField.find { it.x == 0 && it.y == 0 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button2.setOnClickListener() {
-            showGameField(stateField)
-            button2.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 1 && it.y == 0 }
-            stateField.find { it.x == 1 && it.y == 0 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button3.setOnClickListener() {
-            showGameField(stateField)
-            button3.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 2 && it.y == 0 }
-            stateField.find { it.x == 2 && it.y == 0 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button4.setOnClickListener() {
-            showGameField(stateField)
-            button4.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 0 && it.y == 1 }
-            stateField.find { it.x == 0 && it.y == 1 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button5.setOnClickListener() {
-            showGameField(stateField)
-            button5.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 1 && it.y == 1 }
-            stateField.find { it.x == 1 && it.y == 1 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button6.setOnClickListener() {
-            showGameField(stateField)
-            button6.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 2 && it.y == 1 }
-            stateField.find { it.x == 2 && it.y == 1 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button7.setOnClickListener() {
-            showGameField(stateField)
-            button7.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 0 && it.y == 2 }
-            stateField.find { it.x == 0 && it.y == 2 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-        button8.setOnClickListener() {
-            showGameField(stateField)
-            button8.setText(R.string.tic_tac_toe_x)
-            stateField.filter { it.x == 1 && it.y == 2 }
-            stateField.find { it.x == 1 && it.y == 2 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-
-        button9.setOnClickListener() {
-            showGameField(stateField)
-
-            stateField.filter { it.x == 2 && it.y == 2 }
-            stateField.find { it.x == 2 && it.y == 2 }?.state = State.PLAYER
-            showGameField(stateField)
-        }
-
-
-        var stateEmpty = createEmptyState()
-        restartButton.setOnClickListener() {
-            stateEmpty = createEmptyState()
-        }
-    }
-    fun createEmptyState():List<PlayableCell> {
-        return mutableListOf<PlayableCell>(
-        PlayableCell(0, 0, State.EMPTY),
-        PlayableCell(1, 0, State.EMPTY),
-        PlayableCell(2, 0, State.EMPTY),
-        PlayableCell(0, 1, State.EMPTY),
-        PlayableCell(1, 1, State.EMPTY),
-        PlayableCell(2, 1, State.EMPTY),
-        PlayableCell(0, 2, State.EMPTY),
-        PlayableCell(1, 2, State.EMPTY),
-        PlayableCell(2, 2, State.EMPTY)
-        )
     }
 
-    fun showGameField(stateField: MutableList<PlayableCell>) {
+    private fun showGameField() {
         stateField.forEach { cell ->
             if (cell.state == State.PLAYER) {
 
