ccf371d0a0c790b3b19f764a8506e2e360278f9b
nikita <gigtans@gmail.com>
2023-01-30 21:08:29 +0300

    small refactoring code

--- app/src/main/java/com/example/test_project_001/TicTacToeActivity.kt
+++ app/src/main/java/com/example/test_project_001/TicTacToeActivity.kt
@@ -40,56 +40,56 @@ class TicTacToeActivity : AppCompatActivity() {
             stateField.filter { it.x == 0 && it.y == 0 }
             stateField.find { it.x == 0 && it.y == 0 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button2.setOnClickListener() {
             stateField.filter { it.x == 1 && it.y == 0 }
             stateField.find { it.x == 1 && it.y == 0 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button3.setOnClickListener() {
             stateField.filter { it.x == 2 && it.y == 0 }
             stateField.find { it.x == 2 && it.y == 0 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button4.setOnClickListener() {
             stateField.filter { it.x == 0 && it.y == 1 }
             stateField.find { it.x == 0 && it.y == 1 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button5.setOnClickListener() {
             stateField.filter { it.x == 1 && it.y == 1 }
             stateField.find { it.x == 1 && it.y == 1 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button6.setOnClickListener() {
             stateField.filter { it.x == 2 && it.y == 1 }
             stateField.find { it.x == 2 && it.y == 1 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button7.setOnClickListener() {
             stateField.filter { it.x == 0 && it.y == 2 }
             stateField.find { it.x == 0 && it.y == 2 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
         button8.setOnClickListener() {
             stateField.filter { it.x == 1 && it.y == 2 }
             stateField.find { it.x == 1 && it.y == 2 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
 
         button9.setOnClickListener() {
             stateField.filter { it.x == 2 && it.y == 2 }
             stateField.find { it.x == 2 && it.y == 2 }?.state = State.PLAYER
             showGameField()
-            checkVictory()
+            checkVictory(State.PLAYER)
         }
 
         restartButton.setOnClickListener() {
@@ -132,7 +132,7 @@ class TicTacToeActivity : AppCompatActivity() {
         var count: Int = 0
         for (y in 0..2) {
             for (x in 0..2) {
-                if (stateField.find { it.x == x && it.y == y }?.state == State.PLAYER) {
+                if (stateField.find { it.x == x && it.y == y }?.state == typePlayer) {
                     count++
                     println("Count has been added")
                 }
@@ -149,7 +149,7 @@ class TicTacToeActivity : AppCompatActivity() {
 
         for (x in 0..2) {
             for (y in 0..2) {
-                if (stateField.find { it.x == x && it.y == y }?.state == State.PLAYER) {
+                if (stateField.find { it.x == x && it.y == y }?.state == typePlayer) {
                     count++
                     println("Count has been added")
                 }
@@ -163,17 +163,17 @@ class TicTacToeActivity : AppCompatActivity() {
                 count = 0
             }
         }
-        if (stateField.find { it.x == 0 && it.y == 0 }?.state == State.PLAYER &&
-            stateField.find { it.x == 1 && it.y == 1 }?.state == State.PLAYER &&
-            stateField.find { it.x == 2 && it.y == 2 }?.state == State.PLAYER
+        if (stateField.find { it.x == 0 && it.y == 0 }?.state == typePlayer &&
+            stateField.find { it.x == 1 && it.y == 1 }?.state == typePlayer &&
+            stateField.find { it.x == 2 && it.y == 2 }?.state == typePlayer
         ) {
             println("!!! Player 1 won diagonally")
             createEmptyState()
             showGameField()
         }
-        if (stateField.find { it.x == 2 && it.y == 0 }?.state == State.PLAYER &&
-            stateField.find { it.x == 1 && it.y == 1 }?.state == State.PLAYER &&
-            stateField.find { it.x == 0 && it.y == 2 }?.state == State.PLAYER
+        if (stateField.find { it.x == 2 && it.y == 0 }?.state == typePlayer &&
+            stateField.find { it.x == 1 && it.y == 1 }?.state == typePlayer &&
+            stateField.find { it.x == 0 && it.y == 2 }?.state == typePlayer
         ) {
             println("!!! Player 1 won diagonally")
             createEmptyState()
